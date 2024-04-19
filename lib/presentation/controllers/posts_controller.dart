4dd54e27b0d0183afae6eb51f7fc55156d62591d
Kacper Paszek <kakaszi1997@gmail.com>
2024-04-19 11:20:29 +0900

    Initial commit

---
+++ lib/presentation/controllers/posts_controller.dart
@@ -0,0 +1,31 @@
+import 'package:get/get.dart';
+import 'package:getx_architecture/data/models/post_photos_model.dart';
+import 'package:getx_architecture/data/providers/network/model/api_results.dart';
+import 'package:getx_architecture/domain/repositories/post_repository.dart';
+
+class PostController extends GetxController {
+  final PostRepository postRepository;
+  PostController(this.postRepository);
+
+  RxBool loader = false.obs;
+  RxList<PostPhotosModel> photoList = <PostPhotosModel>[].obs;
+
+  Future<void> getPhotosCall() async {
+    loader.value = true;
+    ApiResult result = await postRepository.getPhotos();
+
+    result.when(
+        success: (data, url, headers, statuscode) {
+          photoList.value = postPhotosModelFromJson(data);
+        },
+        error: (data, url, headers, statuscode) {},
+        failure: (networkException) {});
+    loader.value = false;
+  }
+
+  @override
+  void onInit() {
+    getPhotosCall();
+    super.onInit();
+  }
+}
